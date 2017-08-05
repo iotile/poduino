5ebe199ba1b6d60af46be0b522c2f3030e713662
david Karchmer <dkarchmer@gmail.com>
2017-08-05 11:34:25 -0700

    RFID sketch now sending ID to cloud
    Only sending 3 bytes and only when the ID is different from the
    previous ID

--- examples/rfid-example/sketch_rfid/sketch_rfid.ino
+++ examples/rfid-example/sketch_rfid/sketch_rfid.ino
@@ -22,17 +22,48 @@
 // Include IOTile Bridge Library
 #include <IOTileBridgeMega.h>
 
+//***************************************************
+// Usual RFID-RC522 Definitions
+// ----------------------------
 // Define the DIO used for the SDA (SS) and RST (reset) pins.
 #define SDA_DIO 9
 #define RESET_DIO 8
 // Create an instance of the RFID library
 RFID RC522(SDA_DIO, RESET_DIO); 
+//***************************************************
+
+//***************************************************
+// Usual IOTileBridge Definitions
+// ------------------------------
+// Pins connected to IOTile Device
+#define kAttentionPin 39
+#define kEventReceivedPin 3
+// onEvent Callback
+void onEventReceived(unsigned int event);
+// Bridge instantiation
+IOTileBridge bridge(kAttentionPin, onEventReceived);
+// input 10 ==> Sensor Graph will copy to output 0x5001 
+#define kIotileStreamId 10
+//***************************************************
+
+//***************************************************
+// Specific definitions for this example
+
+// Remember last Card detected
+byte last_id[5]={0,0,0,0,0};
+//***************************************************
 
 void setup()
 { 
+  // Start the PODuino Library
+  bridge.begin();
+  
+  // Arduino Debug Terminal
   Serial.begin(9600);
+  
   // Enable the SPI interface
   SPI.begin(); 
+  
   // Initialise the RFID reader
   RC522.init();
 }
@@ -42,16 +73,53 @@ void loop()
   // Has a card been detected?
   if (RC522.isCard())
   {
-    // If so then get its serial number
+    bool new_card_detected = false;
+    
+    // read RFID
     RC522.readCardSerial();
-    Serial.println("Card detected:");
+    
     for(int i=0;i<5;i++)
     {
-    Serial.print(RC522.serNum[i],DEC);
-    Serial.print(RC522.serNum[i],HEX); //to print card detail in Hexa Decimal format
+      if (RC522.serNum[i] != last_id[i])
+      {
+        new_card_detected = true;
+        break;
+      }
     }
-    Serial.println();
-    Serial.println();
+
+    if (new_card_detected)
+    {
+      Serial.println("New Card detected:");
+      for(int i=0;i<5;i++)
+      {
+        // Serial.print(RC522.serNum[i],DEC);
+        Serial.print(RC522.serNum[i],HEX); //to print card detail in Hexa Decimal format
+        if (i < 4) Serial.print("-");
+
+        last_id[i] = RC522.serNum[i];
+      }
+      Serial.println();
+
+      // Send ID to the cloud
+      // We will only send 3 bytes
+      unsigned long value = last_id[2];
+      value = (value << 8) + last_id[3];
+      value = (value << 8) + last_id[4];
+      Serial.println(value, HEX);
+      bridge.sendEvent(kIotileStreamId, value);
+      
+      Serial.println();
+    }
   }
-  delay(1000);
+  bridge.checkReceivedEvents();
+  delay(100);
+}
+
+void onEventReceived(unsigned int event)
+{
+  // Assume the event is the value to set count
+  if ( event == 0 )
+  {
+    Serial.println("Event 0 Received");
+  }
 }
